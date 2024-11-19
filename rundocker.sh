49c9721935d319f5fe0f2c3fb3037f4aeb25265a
pikachu-mini <>
2024-11-19 23:00:54 +0800

    [update] add create tf-app folder; force remove existing tf_workspace

--- rundocker.sh
+++ rundocker.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 clear
 docker rm -f tf_workspace
+mkdir -p ~/SOURCE/tf-app
+docker rm -f tf_workspace
 docker run -dt \
+           -v ~/SOURCE/tf-app:/app \
            --name=tf_workspace \
            --memory=4g \
            tf_workspace:latest \
